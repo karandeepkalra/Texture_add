cbdad3a7de70083c3a9599af52d04264dda96bc6
karandeepkalra <karandeepkaur0089@gmail.com>
2025-04-08 20:14:30 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      'three/addons': 'three/examples/jsm', // Fallback for older paths
+    },
+  },
+  optimizeDeps: {
+    include: ['three', 'three/addons/exporters/GLTFExporter'],
+  },
+});
